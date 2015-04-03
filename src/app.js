a4398b1264d16dc9a3c9653fa7355a88ba2b05aa
drquinn <blong123@gmail.com>
2015-04-02 22:46:12 -0700

    bring in navbar

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 /** @jsx React.Dom */
 
-//var React = require('react');
-//var ShotApp = require('./components/ShotApp.jsx');
-
 import 'babel/polyfill';
 import React from 'react/addons';
 import ShotApp from './components/ShotApp.jsx';
@@ -10,7 +7,6 @@ import $ from 'jquery';
 import mui from 'material-ui';
 
 //var initialState = JSON.parse(document.getElementById('initial-state').innerHTML);
-console.log(React);
 
 let element = React.createElement(ShotApp);
 React.render(element, document.getElementById('react-app'));
--- src/components/ShotApp.jsx
+++ src/components/ShotApp.jsx
@@ -1,12 +1,13 @@
-var React = require('react');
-var AnimalBox = require('./AnimalBox.jsx');
+import React from 'react';
+import AnimalBox from './AnimalBox.jsx';
+import Navbar from './Navbar.jsx';
 
 var ShotApp = React.createClass({
   render: function() {
     return (
       <div className="shot-app">
+        <Navbar />
         <AnimalBox url='/animals' />
-        hi shots
       </div>
     )
   }
