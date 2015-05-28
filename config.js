b962a0bf8e3b62d0dd085eb2f5e3fc490e426e97
drquinn <blong123@gmail.com>
2015-05-28 01:22:58 -0700

    add db and config

---
+++ config.js
@@ -0,0 +1,21 @@
+// gulp configuration
+
+var dest = './build/';
+var src = './src';
+var mui = './node_modules/material-ui/src';
+
+module.exports = {
+    browserify: {
+        dest: dest
+    },
+    webpack: {
+    },
+    less: {
+        src: src + '/less/main.less',
+        dest: dest
+    },
+    markup: {
+        src: src + "/www/**",
+        dest: dest
+    }
+}
